a07f2a0e6cebae756838eda66708053affdf87db
SOUMOBRATA MANNA <148338824+soumoTL@users.noreply.github.com>
2023-10-31 18:15:39 +0530

    Add files via upload

--- Binary_Search_Tree_code.py
+++ Binary_Search_Tree_code.py
@@ -1,47 +1,60 @@
-#Creating a Node
 class Node:
-    def __init__(self,key):
+    def __init__(self, key):
+        self.key = key
         self.left = None
         self.right = None
-        self.val = key
-        
-#Inorder traversal     
-def inordertranversal(root):
-    if root is not None:
-        inordertranversal(root.left)
 
-        print(str(root.key), end = " ")
-        
-        inordertranversal(root.right)
-        
-#Inserting a Node
-def insert(node, key):
-    
-    #Return a new node if the tree is empty
-    if node is None:
-        return node(key)
-    
-    if key <  node.key:
-        node.left = insert(node.left, key)
-    else:
-        node.right = insert(node.right, key)
-        
-    return node
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
 
-#Finding the inorder successor
-def minValueNode(node):
-    current = node
-    
-    #Finding the leftmost node 
-    while(current.left is not None):
-        current=current.left
-    return current
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
 
-#Deleting a Node 
+    def _insert(self, node, key):
+        if node is None:
+            return Node(key)
+        if key < node.key:
+            node.left = self._insert(node.left, key)
+        else:
+            node.right = self._insert(node.right, key) 
+        return node
 
+    def search(self, key):
+        return self._search(self.root, key)
 
-        
-        
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node
+        if key < node.key:
+            return self._search(node.left, key)
+        return self._search(node.right, key)
+
+    def in_order_traversal(self):
+        result = []
+        self._in_order_traversal(self.root, result)
+        return result
+
+    def _in_order_traversal(self, node, result):
+        if node:
+            self._in_order_traversal(node.left, result)
+            result.append(node.key)
+            self._in_order_traversal(node.right, result)
 
 
-        
+bst = BinarySearchTree()
+keys = [50, 30, 70, 20, 40, 60, 80]
+
+for key in keys:
+    bst.insert(key)
+
+print("In-Order Traversal:")
+print(bst.in_order_traversal())
+
+
+search_key = 40
+result = bst.search(search_key)
+if result:
+    print(f"Key {search_key} found.")
+else:
+    print(f"Key {search_key} not found.")
